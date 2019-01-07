16cff77e23a04c008ce7ce37887fdcb6663650fb
Satellite <1090843866@qq.com>
2019-01-07 15:23:23 +0800

    Camera Move
    镜头移动

--- Assets/Resources/Scripts/FightScene/CameraMove.cs
+++ Assets/Resources/Scripts/FightScene/CameraMove.cs
@@ -1,17 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CameraMove : MonoBehaviour
 {
+    private bool isDrag = false;
+    void OnMouseDrag()
+    {
+        //Debug.Log("enter");
+        //if(!EventSystem.current.IsPointerOverGameObject())
+        //{
+        //    isDrag = true;
+        //}
+    }
+    void OnMouseDown()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            isDrag = true;
+        }
+    }
+    void OnMouseUp()
+    {
+        //if (!EventSystem.current.IsPointerOverGameObject())
+        //{
+        //    isDrag = false;
+        //}
+        isDrag = false;
+    }
+    void Update()
+    {
+        if(isDrag)
+        {
+            Camera.main.transform.position += Input.GetAxis("Mouse X") * Vector3.left;
+            if(Camera.main.transform.position.x <= 165.0f)
+            {
+                Camera.main.transform.position = new Vector3(165.0f, Camera.main.transform.position.y, Camera.main.transform.position.z);
+            }
+            if (Camera.main.transform.position.x >= 255.0f)
+            {
+                Camera.main.transform.position = new Vector3(255.0f, Camera.main.transform.position.y, Camera.main.transform.position.z);
+            }
+        }
+    }
 
-	void Start ()
-    {
-	    
-	}
-	
-	void Update ()
-    {
-		
-	}
 }
