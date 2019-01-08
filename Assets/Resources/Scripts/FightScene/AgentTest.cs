bde226ea73e8dfbed96cbc627948d96bfcd5a735
Satellite <1090843866@qq.com>
2019-01-08 19:46:47 +0800

    Complete FightScene
    完善战斗场景，加入AnimatorOverride，以及导航系统

---
+++ Assets/Resources/Scripts/FightScene/AgentTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentTest : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent _agent;
+    private Animator _anim;
+	void Start ()
+    {
+        _anim = this.GetComponent<Animator>();
+        _anim.SetBool("Run", true);
+        _agent = this.GetComponent<NavMeshAgent>();
+        _agent.stoppingDistance = 2.0f;
+        _agent.SetDestination(target.position);
+	}
+	void Update ()
+    {
+		
+	}
+}
